98b80feccbd693837c2df2d3712de88ee8f4d799
iusa <you@example.com>
2024-03-06 22:29:25 +0100

    increased max token size on openai requests + errorhandling

--- agent/jiraAgent/main.py
+++ agent/jiraAgent/main.py
@@ -7,6 +7,8 @@ from langchain.agents import AgentType, initialize_agent
 from langchain_community.agent_toolkits.jira.toolkit import JiraToolkit
 from langchain_community.utilities.jira import JiraAPIWrapper
 from langchain_openai import OpenAI
+from langchain.llms import Ollama
+from langchain_openai import ChatOpenAI
 
 from atlassian import Jira
 
@@ -21,7 +23,8 @@ def executeRequest(req):
     token = os.environ['JIRA_API_TOKEN']
     name = os.environ['JIRA_USERNAME']
     url = os.environ['JIRA_INSTANCE_URL']
-    llm = OpenAI(temperature=0)
+    llm = ChatOpenAI(temperature=0, streaming=True, max_tokens=1000, model_name="gpt-3.5-turbo-16k")
+
     jira = JiraAPIWrapper(jira_api_token=token, jira_instance_url=url, jira_username=name)
     toolkit = JiraToolkit.from_jira_api_wrapper(jira)
     agent = initialize_agent(
@@ -30,7 +33,6 @@ def executeRequest(req):
     res = agent.invoke(req)
     print(res)
     return res
-	
 
 @app.route('/api/v1/request', methods = ['POST'])
 def requestHandler():
@@ -38,14 +40,23 @@ def requestHandler():
     content_type = request.headers.get('Content-Type')
     print(request.headers)
     if (content_type == 'application/json'):
-        print('yeeees')
         if(request.json['agentRequest']):
             receivedAgentRequest =  request.json['agentRequest']
             print(f"request received: {receivedAgentRequest}");
-            return executeRequest(receivedAgentRequest)
-
-    print("oh no")
-    return "request denied"
+            try:
+                result = executeRequest(receivedAgentRequest)
+                print(result)
+                if (result['output']):
+                    return result
+                else:
+                    ret = {}
+                    ret['output'] = "I cannot help you with this right now"
+                    return ret
+            except Exception as e:
+                print(e)
+                ret = {}
+                ret['output'] = "I cannot help you with this right now"
+                return ret
 
 
 if __name__ == '__main__':
